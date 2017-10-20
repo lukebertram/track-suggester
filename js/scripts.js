9f360aaaa6e9b158eb78f73465502caa9309842d
Luke Bertram <lukebertram@gmail.com>
2017-10-20 10:28:47 -0700

    Add survey form to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
 
   $("form#survey").submit(function(event) {
+    var reactScore = 0;
+    var javaScore = 0;
+    var railsScore = 0;
 
+    var answer1 = $("input:radio[name=question1Radios]:checked").val();
+    var answer2 = $("input:radio[name=question2Radios]:checked").val();
+    var answer3 = $("input:radio[name=question3Radios]:checked").val();
+    var answer4 = $("input:radio[name=question4Radios]:checked").val();
+    var answer5 = $("input:radio[name=question5Radios]:checked").val();
 
 
     event.preventDefault();
